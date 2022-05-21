7f4010c9848550c04dd49b3afd4020f18dfd9aaa
Tatiane <tatiane_213@hotmail.com>
2022-05-21 20:25:56 -0300

    tm: create client schema

---
+++ src/clients/clients-model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true,
+  },
+  birthDate: {
+    type: Date,
+    required: true,
+  },
+  cpf: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  postalCode: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    default: 'prefiro não responder',
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Client', schema);
