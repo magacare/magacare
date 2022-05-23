f2844fd90e7e4a2b2f469f50d05f8a253621ab00
Tatiane <tatiane_213@hotmail.com>
2022-05-23 20:25:16 -0300

    tm: refactor validation of joi

--- src/validate/schema-controller.js
+++ src/validate/schema-controller.js
@@ -1,28 +1,29 @@
 const Joi = require('joi');
 
 const ClientSchemaController = Joi.object({
-  fullName: Joi.string().required().min(4),
-  email: Joi.string().required(),
-  birthDate: Joi.string().required(),
-  cpf: Joi.string().max(14),
+  fullName: Joi.string().required().min(4).message('The name of client should have at least 4 characters.'),
+  email: Joi.string().required().email(),
+  birthDate: Joi.string().required('The birth date is required'),
+  cpf: Joi.string().max(14).message('The cpf is required and it should have 14 characters.'),
   phoneNumber: Joi.string().required().min(10).max(14),
-  postalCode: Joi.string().required().min(8),
-  gender: Joi.string().required(),
-  password: Joi.string().required().min(6),
+  postalCode: Joi.string().required().min(8)
+    .message('The postal code should have at least 8 numbers.'),
+  gender: Joi.string().required().valid('mulher cis', 'mulher trans', 'homem cis', 'homem trans', 'não binário', 'prefiro não responder').lowercase(),
+  password: Joi.string().required().min(6).message('The password should have at least 6 characters.'),
 });
 
 const ProductSchemaController = Joi.object({
   code: Joi.string().required(),
-  name: Joi.string().required().min(2).message('O nome do produto deve contrar no minimo 2 caracteres'),
+  name: Joi.string().required().min(2).message('The product name should have at least 2 characters'),
   description: Joi.string().required(),
   volume: Joi.string().required(),
-  recommendation: Joi.string().valid('pele seca', 'pele oleosa', 'pele mista', 'pele normal').required(),
+  recommendation: Joi.string().required().valid('Pele seca', 'Pele oleosa', 'Pele mista', 'Pele normal').lowercase(),
 });
 
 const WishListSchemaController = Joi.object({
-  title: Joi.string().required().min(4).message('O título da wishlist deve contrar no minimo 4 caracteres'),
-  client: Joi.string().required().min(2).message('O id do cliente deve conter pelo menos 2 caracteres'),
-  product: Joi.string().required().min(2).message('O código do produto deve conter pelo menos 2 caracteres'),
+  title: Joi.string().required().min(4).message('The title of wishlist should have at least 4 characters'),
+  client: Joi.string().required().min(2).message('The id of client should have at least 2 characters'),
+  product: Joi.string().required().min(2).message('The code of product should have at least 2 characters'),
 });
 
 module.exports = {
