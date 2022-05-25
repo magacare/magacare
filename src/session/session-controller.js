a8e64a6fcbc2a8ce51d50218334b06c5dc527e62
Tatiane <tatiane_213@hotmail.com>
2022-05-25 19:55:39 -0300

    tm: add treatment of try/catch

--- src/session/session-controller.js
+++ src/session/session-controller.js
@@ -5,31 +5,35 @@ const checkPassword = require('../utils/checkPassword');
 const { verfiyClientExists } = require('./session-service');
 
 const autenticateClient = async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  const client = await verfiyClientExists({ email });
+    const client = await verfiyClientExists({ email });
 
-  if(!client) {
-    return res.status(401).json({ error: 'Client not exist' });
+    if(!client) {
+      return res.status(401).json({ error: 'Client not exist' });
+    }
+
+    if(!(await checkPassword(password, client.password))) {
+      return res.status(401).json({ error: 'Invalid Password' });
+    }
+
+    const { id, name } = client;
+
+    return res.json({
+      client: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+      message: 'Successfully authenticated',
+    });
+  } catch(error) {
+    return res.status(404).json(error.message);
   }
-
-  if(!(await checkPassword(password, client.password))) {
-    return res.status(401).json({ error: 'Invalid Password' });
-  }
-
-  const { id, name } = client;
-
-  return res.json({
-    client: {
-      id,
-      name,
-      email,
-    },
-    token: jwt.sign({ id }, authConfig.secret, {
-      expiresIn: authConfig.expiresIn,
-    }),
-    message: 'Successfully authenticated',
-  });
 };
 
 module.exports = {
